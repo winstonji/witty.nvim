03676afcc4508cb783a8e821cec3c6e016971c1c
winstonji <winston.wy.ji@gmail.com>
2025-09-09 14:40:32 -0600

    testing floating win background color

--- lua/witty/init.lua
+++ lua/witty/init.lua
@@ -94,7 +94,7 @@ function M.setup(config)
 		else
 			vim.api.nvim_win_hide(state.terminal.win)
 		end
-	end, { desc = "Toggle [`<CR>]Terminal emulator" })
+	end, { desc = "Toggle Terminal emulator" })
 
 	-- Open floating terminal
 	vim.keymap.set({ "n", "t" }, float_toggle, function()
@@ -107,7 +107,7 @@ function M.setup(config)
 		else
 			vim.api.nvim_win_hide(state.terminal.win)
 		end
-	end, { desc = "Toggle [F]loating [`<CR>]Terminal emulator" })
+	end, { desc = "Toggle [F]loating Terminal emulator" })
 
 	local term_group = vim.api.nvim_create_augroup("terminal-mode-options", { clear = true })
 
