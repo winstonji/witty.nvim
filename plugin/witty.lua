7b43b39ddca51055f689996d902536c7b29b0e98
winstonji <winston.wy.ji@gmail.com>
2025-09-09 11:15:35 -0600

    testing floating win background color

--- plugin/witty.lua
+++ plugin/witty.lua
@@ -2,10 +2,15 @@ if vim.fn.has("nvim-0.7.0") ~= 1 then
 	vim.api.nvim_echo({ "witty.nvim requires at least nvim-0.7.0.\n" }, true, { "err" })
 end
 
+if vim.g.loaded_witty == 1 then
+	return
+end
+vim.g.loaded_witty = 1
+
 local highlights = {
-	WittyFloat = { default = true, bg = "none" },
+	WittyFloat = { default = true, link = "Normal" },
 	WittyBorder = { default = true, bg = "none" },
-	WittyTitle = { default = true, bg = "none" },
+	WittyTitle = { default = true, link = "Normal" },
 }
 
 for k, v in pairs(highlights) do
