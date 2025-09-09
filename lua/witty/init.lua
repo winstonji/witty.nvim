0eab9ce19fe32ad7489d36acf90b3e561b76a890
winstonji <winston.wy.ji@gmail.com>
2025-09-09 11:31:01 -0600

    testing floating win background color

--- lua/witty/init.lua
+++ lua/witty/init.lua
@@ -122,23 +122,6 @@ function M.setup(config)
 			vim.opt_local.relativenumber = true
 		end,
 	})
-
-	-- local set_hl_for_floating_window = function()
-	-- 	vim.api.nvim_set_hl(0, "NormalFloat", {
-	-- 		link = "Normal",
-	-- 	})
-	-- 	vim.api.nvim_set_hl(0, "FloatBorder", {
-	-- 		bg = "none",
-	-- 	})
-	-- end
-	--
-	-- set_hl_for_floating_window()
-	--
-	-- vim.api.nvim_create_autocmd("ColorScheme", {
-	-- 	pattern = "*",
-	-- 	desc = "Avoid overwritten by loading color schemes later",
-	-- 	callback = set_hl_for_floating_window,
-	-- })
 end
 
 return M
