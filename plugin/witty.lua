f6344e1a6c1ae4c36771897540210cbf376c584e
winstonji <winston.wy.ji@gmail.com>
2025-09-10 10:58:06 -0600

    added loaded checker back in

--- plugin/witty.lua
+++ plugin/witty.lua
@@ -2,10 +2,10 @@ if vim.fn.has("nvim-0.7.0") ~= 1 then
 	vim.api.nvim_echo({ "witty.nvim requires at least nvim-0.7.0.\n" }, true, { "err" })
 end
 
--- if vim.g.loaded_witty == 1 then
--- 	return
--- end
--- vim.g.loaded_witty = 1
+if vim.g.loaded_witty == 1 then
+	return
+end
+vim.g.loaded_witty = 1
 
 local highlights = {
 	WittyNormal = { link = "Normal" },
